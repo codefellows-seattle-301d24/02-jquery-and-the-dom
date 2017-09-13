26506272a55ea33f612fa37acc87b458a6398f5f
kavdihodgson <kavdyjh@gmail.com>
2017-09-13 09:31:56 -0700

    finished third TODO, made .toHTML function

--- kavdi-gabriel/scripts/article.js
+++ kavdi-gabriel/scripts/article.js
@@ -3,7 +3,7 @@
 var articles = [];
 
 function Article (rawDataObj) {
-  // TODO: Use the JS object passed in to complete this constructor function:
+  // TODO:DONE Use the JS object passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
   this.title = rawDataObj.title;
   this.category = rawDataObj.category;
@@ -13,12 +13,17 @@ function Article (rawDataObj) {
   this.body = rawDataObj.body;
 }
 
+/* TODO:DONE This cloned article still has a class of template.
+However, in our modules.css stylesheet, we gave all elements
+with a class of template a display of none. Let's make
+sure we're not accidentally hiding our cloned article! */
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
-  /* TODO: This cloned article still has a class of template.
-  However, in our modules.css stylesheet, we gave all elements
-  with a class of template a display of none. Let's make
-  sure we're not accidentally hiding our cloned article! */
+  $newArticle.attr('class','').attr('data-category', this.category);
+  $newArticle.find('h1.').text(this.title);
+  $newArticle.find('a').text(this.author).attr('href', this.authorUrl);
+  $newArticle.find('time').text(this.publishedOn);
+  $newArticle.find('section').text(this.body);
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.data('category', this.category);
